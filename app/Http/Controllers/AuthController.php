786d68f952a89107f9fb9267da505b8eb8d195a2
rsrmoc <rsrmoc@gmail.com>
2024-04-10 18:58:09 -0300

    08-04-2024

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function home() {
+        return view('login');
+    }
+
     public function login(Request $request) {
         $request->validate([
             'email' => 'required|email',
