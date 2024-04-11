78b56f4cdb917c3fabbb42ee308a2c3fc07ac171
rsrmoc <rsrmoc@gmail.com>
2024-04-11 08:09:27 -0300

    08-04-2024

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use App\Http\Middleware\UserPermissions;
 use App\Models\Perfil;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',  [AuthController::class, 'login']);
+Route::get('/',  [AuthController::class, 'home']);
 Route::get('/login', [Inicio::class, 'login'])->name('login')->middleware('guest');
 route::post('/login', [AuthController::class, 'login'])->name('login-action')->middleware('guest');
 
