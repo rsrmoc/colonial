69aeb18dcda6e4b70ebe4e41576fd1de09d9dbaa
rsrmoc <rsrmoc@gmail.com>
2024-04-24 14:39:36 -0300

    24/04/2024

--- resources/views/colonial/producao/indicadores/previsto_realizado.blade.php
+++ resources/views/colonial/producao/indicadores/previsto_realizado.blade.php
@@ -756,7 +756,7 @@ ul {
                             </template>
 
                             <div class="row">
-                                <div class="col-xs-12 col-sm-8 col-md-8 col-md-offset-2">
+                                <div class="col-xs-12 col-sm-10 col-md-10 col-md-offset-1">
                                     <table class="table">
                                         <thead>
                                             <tr class="active">  
