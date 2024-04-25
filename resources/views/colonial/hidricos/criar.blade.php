5471b3e88a52efd5dc805291a0b6960eb8f70c9f
rsrmoc <rsrmoc@gmail.com>
2024-04-25 08:05:48 -0300

    24/04/2024

--- resources/views/colonial/hidricos/criar.blade.php
+++ resources/views/colonial/hidricos/criar.blade.php
@@ -17,11 +17,11 @@
                     <div class="row">
  
                         <div class="col-md-2">
-                            <div class="form-group @if($errors->has('dt_consumo_anterior')) has-error @endif ">
-                                <label for="fname">Data do Consumo Anterior: <span class="red normal">*</span></label>
-                                <input type="text" class="form-control " readonly   value="{{old('dt_consumo_anterior')}}" placeholder="Data Consumo " name="dt_consumo_anterior"   />
-                                @if($errors->has('dt_consumo_anterior'))
-                                    <div class="error">{{ $errors->first('dt_consumo_anterior') }}</div>
+                            <div class="form-group @if($errors->has('dt_consumo')) has-error @endif ">
+                                <label for="fname">Data do Consumo: <span class="red normal">*</span></label>
+                                <input type="date" class="form-control "     value="{{old('dt_consumo')}}" placeholder="Data Consumo " name="dt_consumo"   />
+                                @if($errors->has('dt_consumo'))
+                                    <div class="error">{{ $errors->first('dt_consumo') }}</div>
                                 @endif
                             </div>
                         </div>
