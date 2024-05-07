7ef31b6ad272385e859679462a681d6372ef273c
rsrmoc <rsrmoc@gmail.com>
2024-05-07 09:50:13 -0300

    07/05/2024

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
 
     public function login(Request $request) {
         $request->validate([
-            'email' => 'required|email',
+            'email' => 'required',
             'password' => 'required|min:8'
         ]);
 
